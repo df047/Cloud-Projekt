0c8cd9ff6dde7d01cd99e2ee36e280d90c66b961
tjard-pueschel <35740468+tjard-pueschel@users.noreply.github.com>
2018-06-04 12:34:44 +0200

    Add files via upload

--- download.php
+++ download.php
@@ -1,43 +1,64 @@
 <?php
 
-// require_once(' php datei für Datenbankverbindung ');
+require_once('sys_class/db_connect.php');
+
 $database = new database();
 $db = $database->get_db();
 $db_connect = $db;
 
 
-// $file_id = $_GET[' Name von der File ID?']; --> hier file_id
-// $user_id = $_GET[' Name von der User ID?']; --> hier user_id
+$file_id = $_GET['file_id'];
+$user_id = $_GET['user_id'];
 
-$get_file = $db_connect->prepare("SELECT * FROM ? WHERE file_id = :file_id");
-$get_file->bindparam(':file_id', $file_id);
+$get_file = $db_connect->prepare("SELECT * FROM tbl_files WHERE file_id = :file_id");
+$get_file->bindparam(':file_id', $file_id);		
 $get_file->execute();
-
+			
 $download_file = $get_file->fetch();
 
-    if($download_file['file_status?'] == 'public')
+if($download_file['file_state'] == 'public'){
 
-        {
+	// Die Datei soll geladen werden
+	$downloadfile = 'file_storage/'.$user_id.'/user_files/'.$file_id.'.'.$download_file['file_type'];	
 
-            // Datei soll heruntergeladen werden
-            $downloadfile = '?file_storage?/'.$user_id.'/user_files/'.$file_id.'.'.$download_file['?file_type?'];
-
-            // Dateiname einer Variablen zuweisen
-            $filename = $download_file['?file_name?'];
-
-            // Dateigröße einer Variable zuweisen
-            $filesize = $download_file['file_size'];
-
-            // Browser Informationen über Datei geben
-            header("Content-Disposition: attachment; file_name='$filename'");
-            header("Content-Length: $filesize");
-
-            // Herunterladen
-            readfile($downloadfile);
-
-            // Final
-            exit;
-
+	// Filename einer Variable zuweisen
+	$filename = $download_file['file_name'];
+		
+	// Filesize einer Variable zuweisen
+	$filesize = $download_file['file_size'];
+	
+	// Browser Infos übergeben zur Datei - mit Werten aus den zuvor definierten Variablen
+	header("Content-Disposition: attachment; filename='$filename'"); 
+	header("Content-Length: $filesize");
+		
+	// Downloaden
+	readfile($downloadfile);
+		
+	// Fertig
+	exit;
+	
 }
 
 else { exit; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
+
+?>
