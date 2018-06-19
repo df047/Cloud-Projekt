dbfb4d1ed8935045df23c539f8e902060577e9f1
tjard-pueschel <35740468+tjard-pueschel@users.noreply.github.com>
2018-06-19 08:19:38 +0200

    Add files via upload

--- download.php
+++ download.php
@@ -1,64 +1,40 @@
 <?php
+//https://mars.iuk.hdm-stuttgart.de/~gurzki/dl/download2.php?filename=bild.jpg
+$directory= "/home/df047/public_html/uploadfiles";
+            require_once "logindaten.php";
 
-require_once('sys_class/db_connect.php');
-
-$database = new database();
-$db = $database->get_db();
-$db_connect = $db;
-
-
-$file_id = $_GET['file_id'];
-$user_id = $_GET['user_id'];
-
-$get_file = $db_connect->prepare("SELECT * FROM tbl_files WHERE file_id = :file_id");
-$get_file->bindparam(':file_id', $file_id);		
-$get_file->execute();
-			
-$download_file = $get_file->fetch();
-
-if($download_file['file_state'] == 'public'){
-
-	// Die Datei soll geladen werden
-	$downloadfile = 'file_storage/'.$user_id.'/user_files/'.$file_id.'.'.$download_file['file_type'];	
-
-	// Filename einer Variable zuweisen
-	$filename = $download_file['file_name'];
-		
-	// Filesize einer Variable zuweisen
-	$filesize = $download_file['file_size'];
-	
-	// Browser Infos übergeben zur Datei - mit Werten aus den zuvor definierten Variablen
-	header("Content-Disposition: attachment; filename='$filename'"); 
-	header("Content-Length: $filesize");
-		
-	// Downloaden
-	readfile($downloadfile);
-		
-	// Fertig
-	exit;
-	
-}
-
-else { exit; }
+            try
+            {
+                $db= new PDO ($dsn,$dbuser,$dbpass);
+            }
+            catch (PDOException $p) {
+                echo("Fehler bei Aufbau der Datenbankverbindung.");
+            }
+            $file=$_GET["fileid"];
+            $sql="SELECT * FROM files WHERE file_id='$file'";
+            $query=$db->prepare($sql);
+            $query->execute();
+            while ($zeile=$query->fetchObject()) {
+                $mimetype=$zeile->mimetype;
+            }
 
 
 
+if (empty($_GET["filename"]))
+    {
+        echo " keine Datei angegeben";
+        die();
+    }
+else
+    {
+        $filename = $_GET["filename"];
+    }
+    $filepath= $directory."/".$filename;
 
 
+header("Content-Type:".$mimetype);
+header('Content-Disposition: attachment;filename="'.$filename.'"');
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: ".filesize($filepath));
+readfile($filepath);
 
-
-
-
-
-
-
-
-
-
-
-
-		
-		
-		
-
-?>
